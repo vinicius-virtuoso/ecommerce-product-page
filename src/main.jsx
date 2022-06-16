54065b377f5a374ec6d490a0e2bf81a6e21e22ce
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2022-06-16 13:29:16 -0300

    rafactor(): removido conflitos com o deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ToastContainer } from "react-toastify";
+
 import App from "./App";
 import Context from "./context";
 import GlobalStyle from "./style/global";
-import "react-toastify/dist/ReactToastify.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Context>
       <GlobalStyle />
       <App />
-      <ToastContainer theme="colored" autoClose={800} />
     </Context>
   </React.StrictMode>
 );
